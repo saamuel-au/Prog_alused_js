09df4ad91e13d2e848cd4e2467b92f79232bed83
saamuel <saamuel.auvaart@voco.ee>
2024-09-05 15:31:01 +0300

    yl3.1

---
+++ yl1.3.js
@@ -0,0 +1,17 @@
+const readline = require("node:readline");
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+
+});
+
+r1.question("sisestage mitu korda äratada", (vastus)=> {
+    function arata() {
+        for (let i =0; i < vastus; i++);
+    }
+}
+
+    arata();
+    r1,Close();
+    });
